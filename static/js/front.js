be830341c312c7517ae811f75403620d1e4d9836
dlmyb <a@dlmyb.com>
2016-08-21 01:35:36 +0800

    fix bugs

--- static/js/front.js
+++ static/js/front.js
@@ -1,5 +1,6 @@
 function runningFunction(){
 	r = $.post('/web/3');
-	setTimeout("javascript:location.href='/web/4'", 3000); 
+	setTimeout("javascript:location.href='/web/4'", 3000);
+	console.log(r.resoponseText);
 	return r.resoponseText;
 }
